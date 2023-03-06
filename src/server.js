1f0101a5eb1b0f702b921e22893055d8e5f7cc7a
Paulo Santos <126355637+paulovictormottasantos@users.noreply.github.com>
2023-03-06 14:50:06 -0300

    feat(account): implement create account route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,35 @@
 const fastify = require('fastify')({ logger:  true });
+const { PrismaClient } = require('@prisma/client');
 
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world.' };
+fastify.post('/account/create', async (request, reply) => {
+  const account = request.body;
+
+  const foundAccount = await prisma.account.findUnique({
+    where: {
+      username: account.username,
+    },
+  });
+
+  if (foundAccount) {
+    reply.code(409).send({ message: 'Username already exists.' });
+  }
+
+  const createdAccount = await prisma.account.create({
+    data: {
+      name: account.name,
+      username: account.username,
+      password: account.password,
+    },
+  });
+
+  if (!createdAccount) {
+    reply.code(500).send({ message: 'Account not created.' });
+  }
+
+  reply.code(201).send({ message: 'Account successfully created.' });
 });
 
 const start = async () => {
